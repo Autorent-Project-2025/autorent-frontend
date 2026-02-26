0a74dcac85e4d017d6b487ff4b887e808bf1e770
Ta1an <alentoleubek@gmail.com>
2026-02-27 04:07:54 +0500

    UPD

--- autorent-frontend/src/main.ts
+++ autorent-frontend/src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./app/router";
 import VueApexCharts from "vue3-apexcharts";
+import VueAxios from "vue-axios";
+import axios from "axios";
 
 import '@/app/assets/main.css'
 import '@/app/assets/animations.css'
@@ -13,5 +15,6 @@ const app = createApp(App);
 app.use(router);
 app.use(createPinia());
 app.use(VueApexCharts);
+app.use(VueAxios, axios);
 
 app.mount("#app");
