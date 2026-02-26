2ced5cd4f102bd3d0ed2262f90d0845ac1c0c6a2
Ta1an <alentoleubek@gmail.com>
2026-02-27 03:58:18 +0500

    Added request for backend(register)

--- autorent-frontend/src/modules/auth/components/RegisterClientForm.vue
+++ autorent-frontend/src/modules/auth/components/RegisterClientForm.vue
@@ -1,27 +1,59 @@
 <script setup lang="ts">
 import Input from "@/shared/ui/Input.vue";
 import Button from "@/shared/ui/Button.vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { register } from "@/app/api/auth";
+import { useToast } from "@/shared/composables/useToast";
+
+const router = useRouter();
+const name = ref("");
+const email = ref("");
+const password = ref("");
+const loading = ref(false);
+const { success, error } = useToast();
+
+async function onRegister() {
+  if (loading.value) return;
+
+  loading.value = true;
+  try {
+    await register(name.value, email.value, password.value);
+    success("Регистрация успешна! Теперь войдите в систему.");
+    router.push("/login");
+  } catch (e: any) {
+    const errorMsg =
+      e.response?.data?.error || "Ошибка при регистрации. Попробуйте снова.";
+    error(errorMsg);
+  } finally {
+    loading.value = false;
+  }
+}
+
 </script>
 
 <template>
-  <form class="flex flex-col gap-4">
+  <form  @submit.prevent="onRegister" class="flex flex-col gap-4">
     <Input
       label="Имя"
       icon="tabler:user-filled"
       placeholder="Ваше имя"
       type="text"
+      v-model="name"
     />
     <Input
       label="Email"
       icon="tabler:mail-filled"
       placeholder="example@mail.com"
       type="email"
+      v-model="email"
     />
     <Input
       label="Пароль"
       icon="tabler:lock-filled"
       placeholder="••••••••"
       type="password"
+      v-model="password"
     />
 
     <Button type="submit">Зарегистрироваться</Button>
